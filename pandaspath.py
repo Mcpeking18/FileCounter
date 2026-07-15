592362590b7a0f50d8f8cfd270bc84da6812ebd1
MCPEKING18 <166641302+Mcpeking18@users.noreply.github.com>
2026-07-16 03:57:20 +0530

    PANDAS ARE PATHING!!! 🗣️🗣️

--- pandaspath.py
+++ pandaspath.py
@@ -2,8 +2,7 @@ import pandas as pd
 from pathlib import Path
 
 def clean(parth,top_dirc):
-    abc = str(parth)
-    abc = abc.removeprefix(f"{str(top_dirc)}\\")
+    abc = str(parth).removeprefix(f"{str(top_dirc)}\\")
     x = abc.find("\\")
     return abc[x+1:]
 
@@ -15,6 +14,8 @@ def main():
         d for d in main_path.iterdir()
         if d.is_dir() and str(d).endswith(".git") == False
     ]
+    #this is the list of directories in main directory
+    #yes its in windows path class not a string
 
     for top_dir in top_dirs:
         
@@ -22,14 +23,15 @@ def main():
             d for d in top_dir.iterdir()
             if d.is_dir()
         ]
+        #now this is list of directories in the one of the directory of main
 
-        with pd.ExcelWriter(f"{top_dir}.xlsx") as Excel:
+        with pd.ExcelWriter(f"{top_dir}.xlsx") as Excel: #to write on excel named {top_dir} ( creates a file if not exist)
 
             for sheet_dir in sheet_dirs:
 
-                peth , name = [],[]
+                peth , name = [],[] #path , name of file
 
-                for item in sheet_dir.rglob("*.dwg"):
+                for item in sheet_dir.rglob("*.dwg"): #find dwg files and append simple
                     peth.append(clean(item.parent,top_dir))
                     name.append(item.name)
 
@@ -37,12 +39,16 @@ def main():
                     "Path" : peth,
                     "Name" : name
                 }
+                #added dictionary to save it all and put in workbook dataframe
 
-                workbook = pd.DataFrame(dic)
+                workbook = pd.DataFrame(dic) #dataframe is created
+
+                #this database will now be inserted into {sheet_dir} named sheet in {top_dir} named excel file
                 workbook.to_excel(
                     Excel, 
                     sheet_name=str(sheet_dir.name),
                     index=False
                 )
         
-main()    
+if __name__ == "__main__":
+    main()
